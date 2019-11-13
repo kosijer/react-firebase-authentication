91a1179026a51ae90c6f02cfeb1276176dcd21cb
neb-sapient <nebojsa.kosijer@publicis.sapient.com>
2019-11-13 11:20:56 +0000

    SignOut menu item

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,4 +1,8 @@
 import React from "react";
-
-const App = () => <div>SignOut</div>;
-export default App;
+import { withFirebase } from "../Firebase";
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
+export default withFirebase(SignOutButton);
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -24,7 +24,7 @@ class SignUpFormBase extends Component {
     this.state = { ...INITIAL_STATE };
   }
   onSubmit = event => {
-    const { username, email, passwordOne } = this.state;
+    const { email, passwordOne } = this.state;
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
